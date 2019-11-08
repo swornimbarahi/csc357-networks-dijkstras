f7ac96c40fdebae494325cf657fa82331ba8b05c
swornimbarahi <sbara002@plattsburgh.edu>
2019-11-08 13:27:41 -0500

    Added files for 3 different mazes

---
+++ djikstras.py
@@ -0,0 +1,3 @@
+from utils import createAdjacencyListWithDistances, printMaze
+
+def djikstras(maze, start):
---
+++ mazes.py
@@ -0,0 +1,7 @@
+def maze(n):
+  if n == 1:
+    return 1
+  if n == 2:
+    return 2
+  if n == 3:
+    return 3
